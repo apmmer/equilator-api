c937bd381f0f1d388e383863d9ef1d5145a27007
Vikentsi <vikentsi.z@northitgroup.eu>
2023-03-21 12:22:54 +0300

    shallow code fixes

--- openapi/modules/auth/exceptions.py
+++ openapi/modules/auth/exceptions.py
@@ -6,3 +6,8 @@ from openapi.core.exceptions import DefaultException
 class InvalidApiKeyError(DefaultException):
     detail: str = "Invalid api_key."
     status_code: status = status.HTTP_401_UNAUTHORIZED
+
+
+class NoApiKeySent(DefaultException):
+    detail: str = "No api-key sent."
+    status_code: status = status.HTTP_403_FORBIDDEN
--- openapi/modules/monitoring/api/endpoints/sentry_check.py
+++ openapi/modules/monitoring/api/endpoints/sentry_check.py
@@ -6,7 +6,11 @@ from openapi.modules.auth.dependencies import verify_api_key
 router = APIRouter()
 
 
-@router.get("/check", status_code=500, dependencies=[Depends(verify_api_key)])
+@router.get(
+    "/check",
+    status_code=500,
+    dependencies=[Depends(verify_api_key)]
+)
 async def trigger_error():
     logger.warning("Raised test sentry exception.")
     raise Exception("Sentry test error.")
